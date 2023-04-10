ded7727e52789f35a5d0edc86dc6dca70d1e2bff
sanjucs12 <sanjucs12@gmail.com>
2023-04-11 00:18:03 +0530

    Added Header and Button

---
+++ src/Layout/Header.js
@@ -0,0 +1,18 @@
+import { Fragment } from "react";
+import foodImage from '../Assets/food.jpg'
+import classes from './Header.module.css'
+import HeaderCartButton from "./HeaderCartButton";
+
+function Header (props){
+    return <Fragment>
+        <header className={classes.header}>
+            <h1>React Foods</h1>
+            <HeaderCartButton />
+        </header>
+        <div className={classes['main-image']}>
+            <img src={foodImage} alt="Delicious food on Table"></img>
+        </div>
+    </Fragment>
+}
+
+export default Header;
