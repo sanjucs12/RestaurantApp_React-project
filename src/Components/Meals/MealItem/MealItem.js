08a8faddfc2e8e50fdddabf9ceb1bf192015aec8
sanjucs12 <sanjucs12@gmail.com>
2023-04-11 13:29:31 +0530

    Form added

--- src/Components/Meals/MealItem/MealItem.js
+++ src/Components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import classes from "./MealItem.module.css";
+import MealItemForm from "./MealItemForm";
 
 function MealItem(props) {
   const price = `$${props.price.toFixed(2)}`;
@@ -9,7 +10,9 @@ function MealItem(props) {
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{price}</div>
       </div>
-      <div></div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 }
